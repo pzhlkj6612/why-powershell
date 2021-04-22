eacdafec73dcb6150bcdfa52f829f207227e6341
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2021-04-22 11:43:37 +0800

    The Magic WMI

--- Find-Processes.ps1
+++ Find-Processes.ps1
@@ -13,6 +13,10 @@
 
 Get-Process | Where-Object { $_.ProcessName -ieq 'PowerShell' } | Format-List Name, Id, Path
 
+# Why don't you ask... the Magic WMI, Squidward?
+#   https://stackoverflow.com/questions/5497064/how-to-get-the-full-path-of-running-process
+#   https://stackoverflow.com/questions/7446887/get-command-line-string-of-64-bit-process-from-32-bit-process
+
 Get-WmiObject Win32_Process | Where-Object { $_.Name -ieq 'PowerShell.exe' } | Format-List Name, ProcessId, Path, ExecutablePath
 # For newer PowerShell
 #   https://devblogs.microsoft.com/scripting/should-i-use-cim-or-wmi-with-windows-powershell/
